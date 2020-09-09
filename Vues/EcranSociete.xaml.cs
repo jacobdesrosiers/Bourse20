c076b5a0e1d91fac9d4121bbf4db162173252c33
jacobdesrosiers <jacobdesrosiers95@gmail.com>
2020-09-09 15:50:13 -0400

    Commit Initial

---
+++ Vues/EcranSociete.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bourses.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour EcranSociete.xaml
+    /// </summary>
+    public partial class EcranSociete : UserControl
+    {
+        public EcranSociete()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdAjouter_Societe(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdModifier_Societe(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdSupprimer_Societe(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdVider_Societe(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
